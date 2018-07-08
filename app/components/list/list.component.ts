80eb9dcd70d55eff23942bf93dfd2aa9b2702263
Adan <twinadanz@gmail.com>
2018-07-08 18:59:01 -0500

    create new list component

--- app/components/list/list.component.ts
+++ app/components/list/list.component.ts
@@ -1,8 +1,27 @@
 import { Component } from "@angular/core";
+import { Location } from "@angular/common"; // Used for subscribing to "pop" events like pushing back button on android device .
+import { Router } from "@angular/router";
+import * as ApplicationSettings from "application-settings";
 
 @Component({
     selector: "list",
-    templateUrl: "app.component.html",
+    templateUrl: "./components/list/list.xml",
 })
 
-export class ListComponent { }
+export class ListComponent { 
+
+    private router: Router;
+    public personList: Array<Object>; // This CAN be rendered in XML file. Private files cannot, as they aren't aware of them.
+
+    constructor(router: Router, location: Location) {
+        this.router = router;
+        this.personList = JSON.parse(ApplicationSettings.getString("people", "[]"));
+        location.subscribe((path) => {
+            this.personList = JSON.parse(ApplicationSettings.getString("people", "[]"));
+        });
+    }
+
+    create() {
+        this.router.navigate(["Create"]);
+    }
+}
