355e03f2fe322f35bcdb2982b3a7d612fb994c62
Adan <twinadanz@gmail.com>
2018-07-08 19:00:07 -0500

    adding imports for ns, config router

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,20 @@
 import { Component } from "@angular/core";
+import { RouteConfig } from "@angular/router";
+import {NS_ROUTER_DIRECTIVES, NS_ROUTER_PROVIDERS} from "nativescript-angular/router";
 
+import {ListComponent} from "./components/list/list.component";
+import {CreateComponent} from "./components/create/create.component";
 @Component({
     selector: "ns-app",
-    templateUrl: "app.component.html",
+    directives: [NS_ROUTER_DIRECTIVES],
+    providers: [NS_ROUTER_PROVIDERS],
+    templateUrl: "app.component.xml",
 })
+@RouteConfig([
+    { path: "/list", compoent: ListComponent, name: "List", useAsDefault: true },
+    { path: '/create', component: CreateComponent, name: "Create"}
+])
 
-export class AppComponent { }
+export class AppComponent { 
+
+}
