089cfa0efe609d41abe97cca4b36cd494e262366
Adan Huerta <Twinadanz@gmail.com>
2018-07-17 23:32:51 -0500

    fixing router to ng 6

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,20 +1,21 @@
 import { Component } from "@angular/core";
-import { RouteConfig } from "@angular/router";
+import { Routes, RouterModule } from "@angular/router";
 import {NS_ROUTER_DIRECTIVES, NS_ROUTER_PROVIDERS} from "nativescript-angular/router";
 
 import {ListComponent} from "./components/list/list.component";
 import {CreateComponent} from "./components/create/create.component";
+
 @Component({
     selector: "ns-app",
     directives: [NS_ROUTER_DIRECTIVES],
     providers: [NS_ROUTER_PROVIDERS],
     templateUrl: "app.component.xml",
 })
-@RouteConfig([
-    { path: "/list", compoent: ListComponent, name: "List", useAsDefault: true },
+const APP_ROUTES: Routes = [
+    { path: "/list", component: ListComponent, name: "List", useAsDefault: true },
     { path: '/create', component: CreateComponent, name: "Create"}
-])
+]
 
-export class AppComponent { 
+export const router = RouterModule.forRoot(APP_ROUTES);
 
-}
+export class AppComponent { }
