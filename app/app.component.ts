be92062608a5058e2fe76aae904d0728d8f2e69f
Adan Huerta <Twinadanz@gmail.com>
2018-07-18 23:26:41 -0500

    reconfigure router to ng6.. still testing

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,21 +1,22 @@
 import { Component } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
-import {NS_ROUTER_DIRECTIVES, NS_ROUTER_PROVIDERS} from "nativescript-angular/router";
+import {NativeScriptRouterModule} from "nativescript-angular/router";
 
 import {ListComponent} from "./components/list/list.component";
 import {CreateComponent} from "./components/create/create.component";
 
 @Component({
     selector: "ns-app",
-    directives: [NS_ROUTER_DIRECTIVES],
-    providers: [NS_ROUTER_PROVIDERS],
-    templateUrl: "app.component.xml",
+    providers: [NativeScriptRouterModule],
+    templateUrl: '<page-router-outlet></page-router-outlet>',
 })
+export class AppComponent { }
+
 const APP_ROUTES: Routes = [
-    { path: "/list", component: ListComponent, name: "List", useAsDefault: true },
-    { path: '/create', component: CreateComponent, name: "Create"}
+    { path: "/list", component: ListComponent },
+    { path: '/create', component: CreateComponent }
 ]
 
 export const router = RouterModule.forRoot(APP_ROUTES);
 
-export class AppComponent { }
+
